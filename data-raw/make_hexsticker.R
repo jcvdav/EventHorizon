d35bea2fe443ebc0aaddcf131053f188de91afec
jcvdav <juancarlos.villader@gmail.com>
2025-08-13 15:58:48 -0400

    Added script to build hexsticker

---
+++ data-raw/make_hexsticker.R
@@ -0,0 +1,12 @@
+library(hexSticker)
+
+sticker(subplot = here::here("man/figures/event_horizon.png"),
+        s_x = 1,
+        s_y = 0.9,
+        package = "EventHorizon",
+        p_size = 20,
+        p_y = 1.4,
+        filename = here::here("man/figures/hexsticker.png"),
+        h_color = "#005040",
+        h_fill = "#005030",
+        asp = 2)
